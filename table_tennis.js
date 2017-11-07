90b120cd0aa1825b5f1fbd1d71f0f07d95b4c022
K15112 <k15112kk@aitech.ac.jp>
2017-11-07 17:40:12 +0900

    4モード推移

--- table_tennis.js
+++ table_tennis.js
@@ -15,6 +15,7 @@ var r_pos = "";
 var r_racket = "";
 var winner = "";
 var playerCount = 1;
+var modecount = 0;
 
 $(function(){
     $('html').keyup(function(e){
@@ -33,30 +34,45 @@ $(function(){
               //     serviceChange = 0;
               //   }
               // }
-              $("#player").val(player);
-              $('#table_body').append(
-                $('<tr>').append(
-                  $('<td>').append(player),
-                  $('<td>').append(s_style),
-                  $('<td>').append(service),
-                  $('<td>').append(s_pos),
-                  $('<td>').append(s_racket),
-                  $('<td>').append(receive),
-                  $('<td>').append(r_style),
-                  $('<td>').append(r_pos),
-                  $('<td>').append(r_racket),
-                  $('<td>').append(winner)
+
+              if(modecount == 0){
+                  modecount++;
+                  console.log("1");
+              }else if(modecount == 1){
+                  modecount++;
+                  console.log("2");
+              }else if(modecount == 2){
+                  modecount++;
+                  console.log("3");
+              }else{
+                  console.log("4");
+
+                $("#player").val(player);
+                $('#table_body').append(
+                  $('<tr>').append(
+                    $('<td>').append(player),
+                    $('<td>').append(s_style),
+                    $('<td>').append(service),
+                    $('<td>').append(s_pos),
+                    $('<td>').append(s_racket),
+                    $('<td>').append(receive),
+                    $('<td>').append(r_style),
+                    $('<td>').append(r_pos),
+                    $('<td>').append(r_racket),
+                    $('<td>').append(winner)
+                  )
                 )
-              )
-              $("#service").val("");
-              $("#s_style").val("");
-              $("#s_pos").val("");
-              $("#s_racket").val("");
-              $("#receive").val("");
-              $("#r_style").val("");
-              $("#r_pos").val("");
-              $("#r_racket").val("");
-              $("#winner").val("");
+                $("#service").val("");
+                $("#s_style").val("");
+                $("#s_pos").val("");
+                $("#s_racket").val("");
+                $("#receive").val("");
+                $("#r_style").val("");
+                $("#r_pos").val("");
+                $("#r_racket").val("");
+                $("#winner").val("");
+                modecount = 0;
+              }
             break;
 
             // case 9: // Tab
@@ -93,127 +109,256 @@ $(function(){
             //改善版
             //サービステクニック 1-7
             case 49: //1
-                $("#winner").val("0");
+                if(modecount == 0){
+                  $("#s_style").val("0");
+                }else if(modecount == 1){
+
+                }else if(modecount == 2){
+
+                }else{
+
+                }
             break;
             case 50: //2
+              if(modecount == 0){
                 $("#s_style").val("0");
+
+              }else if(modecount == 1){
+
+
+              }else if(modecount == 2){
+
+
+              }else{
+
+
+              }
             break;
             case 51: //3
-                $("#s_style").val("1");
+              if(modecount == 0){
+                $("#s_style").val("0");
+
+              }else if(modecount == 1){
+
+
+              }else if(modecount == 2){
+
+
+              }else{
+
+
+              }
             break;
             case 52: //4
-                $("#s_style").val("2");
+              if(modecount == 0){
+                $("#s_style").val("0");
+
+              }else if(modecount == 1){
+
+
+              }else if(modecount == 2){
+
+
+              }else{
+
+
+              }
             break;
             case 53: //5
-                $("#s_style").val("3");
+              if(modecount == 0){
+                $("#s_style").val("0");
+
+              }else if(modecount == 1){
+
+
+              }else if(modecount == 2){
+
+
+              }else{
+
+
+              }
             break;
             case 54: //6
-                $("#s_style").val("4");
+              if(modecount == 0){
+                $("#s_style").val("0");
+
+              }else if(modecount == 1){
+
+
+              }else if(modecount == 2){
+
+
+              }else{
+
+
+              }
             break;
-            //トス高さ
-            case 69: //W
-                $("#s_toss").val("0");
-            break;
-            case 68: //S
-                $("#s_toss").val("1");
-            break;
-            case 67: //X
-                $("#s_toss").val("2");
-            break;
-            //コース
-            case 83: //E
-                $("#s_pos").val("0");
-            break;
-            case 87: //D
-                $("#s_pos").val("3");
-            break;
-            case 88: //C
-                $("#s_pos").val("6");
-            break;
-            case 83: //R
-                $("#s_pos").val("1");
-            break;
-            case 87: //F
-                $("#s_pos").val("4");
-            break;
-            case 88: //V
-                $("#s_pos").val("7");
-            break;
-            case 83: //T
-                $("#s_pos").val("2");
-            break;
-            case 87: //G
-                $("#s_pos").val("5");
-            break;
-            case 88: //B
-                $("#s_pos").val("8");
-            break;
-            //レシーブ
             case 55: //7
-                $("#r_style").val("0");
+              if(modecount == 0){
+                $("#s_style").val("0");
+
+              }else if(modecount == 1){
+
+
+              }else if(modecount == 2){
+
+
+              }else{
+
+
+              }
             break;
             case 56: //8
-                $("#r_style").val("1");
+              if(modecount == 0){
+                $("#s_style").val("0");
+
+              }else if(modecount == 1){
+
+
+              }else if(modecount == 2){
+
+
+              }else{
+
+
+              }
             break;
             case 57: //9
-                $("#r_style").val("2");
+              if(modecount == 0){
+                $("#s_style").val("0");
+
+              }else if(modecount == 1){
+
+
+              }else if(modecount == 2){
+
+
+              }else{
+
+
+              }
             break;
             case 48: //0
-                $("#r_style").val("3");
+              if(modecount == 0){
+                $("#s_style").val("0");
+
+              }else if(modecount == 1){
+
+
+              }else if(modecount == 2){
+
+
+              }else{
+
+
+              }
             break;
-            case 189: //-
-                $("#r_style").val("4");
-            break;
-            case 89: //Y
-                $("#r_style").val("5");
-            break;
-            case 85: //U
-                $("#r_style").val("6");
-            break;
-            case 72: //H
-                $("#r_style").val("7");
-            break;
-            case 78: //N
-                $("#r_style").val("8");
-            break;
-            case 74: //J
-                $("#r_racket").val("0");
-            break;
-            case 77: //M
-                $("#r_racket").val("1");
-            break;
-            //コース
-            case 73: //I
-                $("#r_pos").val("0");
-            break;
-            case 75: //K
-                $("#r_pos").val("3");
-            break;
-            case 188: //<
-                $("#r_pos").val("6");
-            break;
-            case 79: //O
-                $("#r_pos").val("1");
-            break;
-            case 76: //L
-                $("#r_pos").val("4");
-            break;
-            case 190: //>
-                $("#r_pos").val("7");
-            break;
-            case 80: //P
-                $("#r_pos").val("2");
-            break;
-            case 186: //;
-                $("#r_pos").val("5");
-            break;
-            case 191: //?
-                $("#r_pos").val("8");
-            break;
-            case 191: //alt
-                $("#r_pos").val("-2");
-                $("#r_style").val("-2");
-            break;
+
+            // //トス高さ
+            // case 69: //W
+            //     $("#s_toss").val("0");
+            // break;
+            // case 68: //S
+            //     $("#s_toss").val("1");
+            // break;
+            // case 67: //X
+            //     $("#s_toss").val("2");
+            // break;
+            // //コース
+            // case 83: //E
+            //     $("#s_pos").val("0");
+            // break;
+            // case 87: //D
+            //     $("#s_pos").val("3");
+            // break;
+            // case 88: //C
+            //     $("#s_pos").val("6");
+            // break;
+            // case 83: //R
+            //     $("#s_pos").val("1");
+            // break;
+            // case 87: //F
+            //     $("#s_pos").val("4");
+            // break;
+            // case 88: //V
+            //     $("#s_pos").val("7");
+            // break;
+            // case 83: //T
+            //     $("#s_pos").val("2");
+            // break;
+            // case 87: //G
+            //     $("#s_pos").val("5");
+            // break;
+            // case 88: //B
+            //     $("#s_pos").val("8");
+            // break;
+            // //レシーブ
+            // case 55: //7
+            //     $("#r_style").val("0");
+            // break;
+            // case 56: //8
+            //     $("#r_style").val("1");
+            // break;
+            // case 57: //9
+            //     $("#r_style").val("2");
+            // break;
+            // case 48: //0
+            //     $("#r_style").val("3");
+            // break;
+            // case 189: //-
+            //     $("#r_style").val("4");
+            // break;
+            // case 89: //Y
+            //     $("#r_style").val("5");
+            // break;
+            // case 85: //U
+            //     $("#r_style").val("6");
+            // break;
+            // case 72: //H
+            //     $("#r_style").val("7");
+            // break;
+            // case 78: //N
+            //     $("#r_style").val("8");
+            // break;
+            // case 74: //J
+            //     $("#r_racket").val("0");
+            // break;
+            // case 77: //M
+            //     $("#r_racket").val("1");
+            // break;
+            // //コース
+            // case 73: //I
+            //     $("#r_pos").val("0");
+            // break;
+            // case 75: //K
+            //     $("#r_pos").val("3");
+            // break;
+            // case 188: //<
+            //     $("#r_pos").val("6");
+            // break;
+            // case 79: //O
+            //     $("#r_pos").val("1");
+            // break;
+            // case 76: //L
+            //     $("#r_pos").val("4");
+            // break;
+            // case 190: //>
+            //     $("#r_pos").val("7");
+            // break;
+            // case 80: //P
+            //     $("#r_pos").val("2");
+            // break;
+            // case 186: //;
+            //     $("#r_pos").val("5");
+            // break;
+            // case 191: //?
+            //     $("#r_pos").val("8");
+            // break;
+            // case 191: //alt
+            //     $("#r_pos").val("-2");
+            //     $("#r_style").val("-2");
+            // break;
             case 49: //1
                 if($("#player").val() == "player2_id"){
                   $("#player").val("player1_id");
