c599797e5333d02b1e1812ef2275d28a0561296e
K15112 <k15112kk@aitech.ac.jp>
2017-12-13 11:42:04 +0900

    入力の色変更、レイアウト修正

--- table_tennis.js
+++ table_tennis.js
@@ -1,8 +1,9 @@
 
 
-var player1_id = "0";
-var player2_id = "1";
+var player1_id = "PlayerA";
+var player2_id = "PlayerB";
 $("#service").val(player1_id);
+
 var s_style = "";
 var s_pos = "0";
 var s_racket = "";
@@ -17,6 +18,8 @@ var modecount = 0;
 
 $("#s_pos").val("0");
 $("#r_pos").val("0");
+$("#s_racketColor").css("background-color","#FF9900");
+$("#s_styleColor").css("background-color","#FF9900");
 
 document.getElementById("PPAP").src = "PPAPS.jpg";
 document.getElementById("s_pos").src = "./img/" + $("#s_pos").val() + ".png";
@@ -27,38 +30,43 @@ $(function(){
         switch(e.which){
             case 13: // Enter
               if(modecount == 0){
+                $("#s_racketColor").css("background-color","#006666");
+                $("#s_styleColor").css("background-color","#006666");
+                $("#s_posColor").css("background-color","#FF9900");
                   modecount++;
               }else if(modecount == 1){
+                $("#s_posColor").css("background-color","#006666");
+                $("#r_racketColor").css("background-color","#FF9900");
+                $("#r_styleColor").css("background-color","#FF9900");
                   modecount++;
               }else if(modecount == 2){
+                $("#r_racketColor").css("background-color","#006666");
+                $("#r_styleColor").css("background-color","#006666");
+                $("#r_posColor").css("background-color","#FF9900");
                   modecount++;
               }else{
-                console.log($("#service").val());
-                // console.log($("#s_racket").val());
-                // console.log($("#s_style").val());
-                // console.log($("#s_pos").val());
-                // console.log($("#r_racket").val());
-                // console.log($("#r_style").val());
-                // console.log($("#r_pos").val());
-
+                $("#r_posColor").css("background-color","#006666");
+                $("#s_racketColor").css("background-color","#FF9900");
+                $("#s_styleColor").css("background-color","#FF9900");
                 $('#table_body').append(
                   $('<tr>').append(
                     $('<td width="30">').append($("#service").val()),
                     $('<td width="10">').append($("#s_racket").val()),
                     $('<td width="100">').append($("#s_style").val()),
-                    //$('<td>').append($("<img alt="s_pos" width="50" height="30">")),
-                    $("#s_pos").clone(true).insertAfter(table_body),
+                    $('<td width="50">').append('<img src="./img/' + $("#s_pos").val() + '.png" width="50" height="38">'),
+                    //$("#s_pos").clone(true).insertAfter(table_body),
                     $('<td width="10">').append($("#r_racket").val()),
                     $('<td width="100">').append($("#r_style").val()),
-                    $("#r_pos").clone(true).insertAfter(table_body),
+                    $('<td width="50">').append('<img src="./img/' + $("#r_pos").val() + '.png" width="50" height="38">'),
+                    //$("#r_pos").clone(true).insertAfter(table_body),
                   )
                 )
                 $("#service").val("");
                 $("#s_style").val("");
-                $("#s_pos").val("");
+                $("#s_pos").val("0");
                 $("#s_racket").val("");
                 $("#r_style").val("");
-                $("#r_pos").val("");
+                $("#r_pos").val("0");
                 $("#r_racket").val("");
                 modecount = 0;
                 if(playerCount == 0){
@@ -68,39 +76,12 @@ $(function(){
                   playerCount = 0;
                   $("#service").val(player2_id);
                 }
+                document.getElementById("s_pos").src = "./img/0.png";
+                document.getElementById("r_pos").src = "./img/0.png";
               }
+
             break;
 
-            // case 9: // Tab
-            //   larryCount = larryCount + 1;
-            //   $('tr:last').append(
-            //       $('<td>').append(receive),
-            //       $('<td>').append(side),
-            //       $('<td>').append(rform),
-            //       $('<td>').append(rcourse),
-            //   )
-            //   if(headCount < larryCount){
-            //     headCount = larryCount;
-            //     var li = "<th>" + headCount + "打球目</th>"
-            //     //$('oppai5').text(li);               )
-            //     $('#oppai').append(
-            //       $('<th colspan="4">').append(li)
-            //     )
-            //     $('#oppai2').append(
-            //       $('<th>').append("<th>打球位置</th>"),
-            //       $('<th>').append("<th>ラケット面</th>"),
-            //       $('<th>').append("<th>打法</th>"),
-            //       $('<th>').append("<th>コース</th>"),
-            //     )
-            //     /*$('#oppai3').append(
-            //       $('<th>').append("<th><input type="text" size="8" maxlength="8" name="receive" value=""></th>"),
-            //       $('<th>').append("<th><input type="text" size="10" maxlength="10" name="side" value=""></th>"),
-            //       $('<th>').append("<th><input type="text" size="3" maxlength="3" name="form" value=""></th>"),
-            //       $('<th>').append("<th><input type="text" size="6" maxlength="6" name="course" value=""></th>"),
-            //     )*/
-            //   }
-            // break;
-            //キー入力の実装
             case 49: //1
                 if(modecount == 0){
                   $("#s_style").val("横回転");
@@ -257,132 +238,16 @@ $(function(){
                 i++;
                 $("#table_body").    //#table_bodyを取得
                   clone(true).   //上で取得した中身をコピー
-                    find('.detail').  //そのコピーした中身のdetailとついている要素を取得
+                    find('.score').  //そのコピーした中身のdetailとついている要素を取得
                       attr('id',i).  //上で取得した要素の中のidをiとする
                         appendTo("#roots");  //#rootsに追加
               }
+              $("#td").css("margin","2px");
             break;
-
-
-            // //トス高さ
-            // case 69: //W
-            //     $("#s_toss").val("0");
-            // break;
-            // case 68: //S
-            //     $("#s_toss").val("1");
-            // break;
-            // case 67: //X
-            //     $("#s_toss").val("2");
-            // break;
-            // //コース
-            // case 83: //E
-            //     $("#s_pos").val("0");
-            // break;
-            // case 87: //D
-            //     $("#s_pos").val("3");
-            // break;
-            // case 88: //C
-            //     $("#s_pos").val("6");
-            // break;
-            // case 83: //R
-            //     $("#s_pos").val("1");
-            // break;
-            // case 87: //F
-            //     $("#s_pos").val("4");
-            // break;
-            // case 88: //V
-            //     $("#s_pos").val("7");
-            // break;
-            // case 83: //T
-            //     $("#s_pos").val("2");
-            // break;
-            // case 87: //G
-            //     $("#s_pos").val("5");
-            // break;
-            // case 88: //B
-            //     $("#s_pos").val("8");
-            // break;
-            // //レシーブ
-            // case 55: //7
-            //     $("#r_style").val("0");
-            // break;
-            // case 56: //8
-            //     $("#r_style").val("1");
-            // break;
-            // case 57: //9
-            //     $("#r_style").val("2");
-            // break;
-            // case 48: //0
-            //     $("#r_style").val("3");
-            // break;
-            // case 189: //-
-            //     $("#r_style").val("4");
-            // break;
-            // case 89: //Y
-            //     $("#r_style").val("5");
-            // break;
-            // case 85: //U
-            //     $("#r_style").val("6");
-            // break;
-            // case 72: //H
-            //     $("#r_style").val("7");
-            // break;
-            // case 78: //N
-            //     $("#r_style").val("8");
-            // break;
-            // case 74: //J
-            //     $("#r_racket").val("0");
-            // break;
-            // case 77: //M
-            //     $("#r_racket").val("1");
-            // break;
-            // //コース
-            // case 73: //I
-            //     $("#r_pos").val("0");
-            // break;
-            // case 75: //K
-            //     $("#r_pos").val("3");
-            // break;
-            // case 188: //<
-            //     $("#r_pos").val("6");
-            // break;
-            // case 79: //O
-            //     $("#r_pos").val("1");
-            // break;
-            // case 76: //L
-            //     $("#r_pos").val("4");
-            // break;
-            // case 190: //>
-            //     $("#r_pos").val("7");
-            // break;
-            // case 80: //P
-            //     $("#r_pos").val("2");
-            // break;
-            // case 186: //;
-            //     $("#r_pos").val("5");
-            // break;
-            // case 191: //?
-            //     $("#r_pos").val("8");
-            // break;
-            // case 191: //alt
-            //     $("#r_pos").val("-2");
-            //     $("#r_style").val("-2");
-            // break;
-            // case 49: //1
-            //     if($("#player").val() == "player2_id"){
-            //       $("#player").val("player1_id");
-            //     }else {
-            //       $("#player").val("player2_id");
-            //       playerCount = 1;
-            //     }
-            // break;
 
         }
     });
 });
-
-
-
 
 function movieChange(){
   //inputフィールドのテキスト内容を文字列として取得
@@ -413,11 +278,3 @@ function splitString (stringToSplit,separator) {
   //分割した文字列の配列5番目にurlが入っているのでリターン
   return arrayOfStrings[5];
 }
-
-// 作成するテーブルの定義
- var r_end = 5;  // 行数
- var c_end = 4;  // 列数
-
-/*------------------------------------------------
- jQuery で動的にテーブルを作成する
---------------------------------------------------*/
